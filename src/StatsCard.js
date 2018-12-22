e9f1f90714106861b074a84d7dd69f1b18f713cc
Mateusz Bielak <mateusz.bielak@skygate.io>
2018-12-22 22:41:44 +0100

    Remove unused variables

--- src/StatsCard.js
+++ src/StatsCard.js
@@ -2,20 +2,15 @@ import React from 'react';
 import { Card } from 'antd';
 import { SelectStation } from './SelectStation';
 
-export const StatsCard = ({ fetchStationIndex, stations, stationIndex }) => {
-    const station = stationIndex && stations.find(({ id }) => id === stationIndex.id);
-    console.log(stationIndex);
-
-    return (
-        <Card
-            title={<SelectStation stations={stations} fetchStationIndex={fetchStationIndex} />}
-            style={{ width: 300 }}
-        >
-            <span style={{ fontWeight: '600', color: '#fff' }}>
-                {stationIndex
-                    ? `Stan powietrza: ${stationIndex.stIndexLevel.indexLevelName}`
-                    : 'Jakie mamy dzisiaj powietrze?'}
-            </span>
-        </Card>
-    );
-};
+export const StatsCard = ({ fetchStationIndex, stations, stationIndex }) => (
+    <Card
+        title={<SelectStation stations={stations} fetchStationIndex={fetchStationIndex} />}
+        style={{ width: 300 }}
+    >
+        <span style={{ fontWeight: '600', color: '#fff' }}>
+            {stationIndex
+                ? `Stan powietrza: ${stationIndex.stIndexLevel.indexLevelName}`
+                : 'Jakie mamy dziś powietrze?'}
+        </span>
+    </Card>
+);
